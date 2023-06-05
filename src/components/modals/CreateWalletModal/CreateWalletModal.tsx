769a3d2717344b7ab575046836a50e1fc9fcb098
morozov312 <alexander.morozov312@gmail.com>
2023-06-06 04:30:22 +0700

    chore: add types and interfaces folder

--- src/components/modals/CreateWalletModal/CreateWalletModal.tsx
+++ src/components/modals/CreateWalletModal/CreateWalletModal.tsx
@@ -1,7 +1,19 @@
+import Modal from '@ui/Modal/Modal';
 import React from 'react';
+import { IModalProps } from 'ts/interfaces/modal';
 
-const CreateWalletModal = () => {
-  return <></>;
+const CreateWalletModal = ({
+  showModalState,
+  showModalSetStateAction,
+}: IModalProps) => {
+  return (
+    <Modal
+      showModalState={showModalState}
+      showModalSetStateAction={showModalSetStateAction}
+    >
+      {}
+    </Modal>
+  );
 };
 
 export default CreateWalletModal;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import CreateWalletModal from 'components/modals/CreateWalletModal/CreateWalletModal';
+import React, { useState } from 'react';
 
 const Home = () => {
+  const [showCreateWalletModal, setShowCreateWalletModal] =
+    useState<boolean>(false);
+
   return (
     <div className='flex flex-col justify-center mt-20 text-center gap-7'>
+      <CreateWalletModal
+        showModalState={showCreateWalletModal}
+        showModalSetStateAction={setShowCreateWalletModal}
+      />
       <h1>Добро пожаловать!</h1>
       <span>Импортируйте существующий кошелек или создайте новый</span>
       <div className='flex justify-between w-1/4 mx-auto'>
-        <button className='btn-primary'>Создать</button>
+        <button
+          onClick={() => setShowCreateWalletModal(true)}
+          className='btn-primary'
+        >
+          Создать
+        </button>
         <button className='btn-primary'>Импорт</button>
       </div>
     </div>
