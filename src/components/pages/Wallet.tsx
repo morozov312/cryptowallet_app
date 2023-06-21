ada16972f329f5400b1f037ff99e7c6c88bc0161
morozov312 <alexander.morozov312@gmail.com>
2023-06-22 06:41:38 +0700

    feature: add loader

--- src/components/pages/Wallet.tsx
+++ src/components/pages/Wallet.tsx
@@ -17,27 +17,32 @@ const Wallet = () => {
   const [showWalletModal, setShowWalletModal] = useState<boolean>(false);
   const [sendModal, setSendModal] = useState<boolean>(false);
 
-  const calcBalance = useCallback(async (): Promise<string> => {
-    const balance = ((await provider?.getBalance(wallet?.address || '')) ||
+  const calcBalance = useCallback(async (): Promise<BigNumber> => {
+    return ((await provider?.getBalance(wallet?.address || '')) ||
       0) as BigNumber;
-    const { data: pricesArr } = await axios.get(
-      'https://api.coinbase.com/v2/exchange-rates?currency=ETH',
-    );
-    const formattedBalance = utils.formatEther(balance);
-
-    return (pricesArr.data.rates.USD * Number(formattedBalance)).toString();
   }, [provider, wallet?.address]);
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const balance = await calcBalance();
-        setUserUSDBalance(balance);
-      } catch (e: any) {
-        toast.error(e.message);
-      }
-    })();
+  const refreshBalance = useCallback(async () => {
+    try {
+      setUserETHBalance('');
+      setUserUSDBalance('');
+      const balance = await calcBalance();
+      const formattedBalance = utils.formatEther(balance);
+      const { data: pricesArr } = await axios.get(
+        'https://api.coinbase.com/v2/exchange-rates?currency=ETH',
+      );
+      setUserUSDBalance(
+        (pricesArr.data.rates.USD * Number(formattedBalance)).toFixed(4),
+      );
+      setUserETHBalance(formattedBalance);
+    } catch (e: any) {
+      toast.error(e.message);
+    }
   }, [calcBalance]);
+
+  useEffect(() => {
+    refreshBalance().catch(console.error);
+  }, [refreshBalance]);
 
   useEffect(() => {
     if (!provider || !wallet) {
@@ -45,14 +50,10 @@ const Wallet = () => {
     }
   }, [navigate, provider, wallet]);
 
-  if (provider === null || wallet === null) {
-    return null;
-  }
-
   return (
     <div className='flex flex-col justify-center align-middle items-center p-10 gap-16'>
       <ShowWalletModal
-        address={wallet.address}
+        address={wallet?.address || ''}
         showModalState={showWalletModal}
         showModalSetStateAction={setShowWalletModal}
       />
@@ -60,7 +61,13 @@ const Wallet = () => {
         showModalState={sendModal}
         showModalSetStateAction={setSendModal}
       />
-      <span className='text-4xl'>${userUSDBalance}</span>
+      {userETHBalance.length ? (
+        <span className='text-4xl'>
+          ETH {userETHBalance} / $ {userUSDBalance}
+        </span>
+      ) : (
+        <span className='animate-pulse'>Загрузка баланса</span>
+      )}
       <div className='flex justify-between w-1/3'>
         <button onClick={() => setSendModal(true)} className='btn-primary'>
           Отправить
@@ -70,6 +77,9 @@ const Wallet = () => {
           className='btn-primary'
         >
           Получить
+        </button>
+        <button onClick={refreshBalance} className='btn-primary'>
+          Обновить баланс
         </button>
       </div>
     </div>
